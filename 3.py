8a2fa43fecfe598e7cf9c11f85baa00ea9610728
themaximsuper <41567448+themaximsuper@users.noreply.github.com>
2025-08-18 19:18:11 +0300

    Some optimizations

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
-N = int(input())
+n = int(input())
 m = list(map(int, input().split()))
 
-first_half, second_half = m[:N//2], m[N//2:]
+first_half, second_half = m[:n // 2], m[n // 2:]
 
-for i in range(N//2):
-    first_half[0+i], first_half[-1-i] = m[-1-N//2-i], m[0+i]
-    second_half[0+i], second_half[-1-i] = m[-1-i], m[0+N//2+i]
+for i in range(int(n / 4)):
+    first_half[i], first_half[- 1 - i] = m[- 1 - n // 2 - i], m[i]
+    second_half[i], second_half[- 1 - i] = m[- 1 - i], m[n // 2 + i]
 
-print(" ".join(str(num) for num in first_half+second_half))
+print(" ".join(str(num) for num in first_half + second_half))
